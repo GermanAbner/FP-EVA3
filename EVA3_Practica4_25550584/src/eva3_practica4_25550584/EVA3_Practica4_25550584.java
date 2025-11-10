b3fe71ab1a117ef77ed91791e70a94ae410756ca
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-11-10 15:52:19 -0700

    EVA3_Practica4_25550584

---
+++ EVA3_Practica4_25550584/src/eva3_practica4_25550584/EVA3_Practica4_25550584.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_practica4_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA3_Practica4_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor = 5;
+        System.out.println("valor (antes) = " + valor);
+        incrementar(valor);
+        System.out.println("valor (ahora) = " + valor);
+    }
+    public static void incrementar(int val){
+    //se manda una copia mas no es la originall, una evs editada la copia no se va a editar la originall
+                //OBVIO PAPA OBVIO
+         val =+ 1;
+         //valor++
+        
+    }
+    
+}
