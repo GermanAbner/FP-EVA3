9df77eeb26831376fef1b48b4da15d9632b98616
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-11-11 15:52:07 -0700

    EVA3_Practica8_25550584

---
+++ EVA3_Practica8_25550584/src/eva3_practica8_25550584/EVA3_Practica8_25550584.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_practica8_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA3_Practica8_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println(100);
+        System.out.println(100.0);
+        System.out.println(true);
+        System.out.println('o');
+        System.out.println(args);
+        
+    }
+    
+}
